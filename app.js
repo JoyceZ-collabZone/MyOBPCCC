c954d3f97861211f61411940be0f4d86c355d0c5
joyce <joyce.zhou@westpac.com.au>
2020-05-30 12:34:40 +1000

    left hand side is working now

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+// define product at the top, scoping, variable is visible in functions, inside variables first before reading outside
+let products = [];
+
 async function WestpacProductAPI() {
+  // let products = null;
   const response = await $.ajax({
     url: "https://digital-api.westpac.com.au/cds-au/v1/banking/products",
     method: "get",
@@ -6,6 +10,7 @@ async function WestpacProductAPI() {
       Accept: "application/json",
       "x-v": 1,
       "x-min-v": 1,
+      // "Access-Control-Allow-Origin": "*",
     },
   });
   return response; // function (response as parameter)
@@ -13,10 +18,10 @@ async function WestpacProductAPI() {
 const apiPromise = WestpacProductAPI();
 apiPromise.then((response) => {
   console.log(response);
-  let arrayOfWBCProduct = response.data.products;
+  products = response.data.products;
   // console.log(arrayOfWBCProduct);
   // console.log(JSON.stringify(response));
-  returnSingleArrayItem(arrayOfWBCProduct);
+  returnSingleArrayItem(products);
 });
 
 function returnSingleArrayItem(products) {
@@ -32,25 +37,13 @@ function returnSingleArrayItem(products) {
     const productListingTag = $(
       `<option value="${product.productId}">${product.productId}</option>`
     );
+
+    console.log("testing is product name is returned " + product.name);
+
     // const productListing = $(`<div id="${product.productID}"></div>`);
 
     console.log("product listing logging: " + productListingTag);
     $("#productSelection").append(productListingTag);
-
-    // const productSummaryInfoTag = $("#productSummaryView").append(
-    //   `<li>${product.name}</li>`
-    // );
-
-    // const singleProductID = singleWBCProduct.productId;
-    // const singleProductCategory = singleWBCProduct.productCategory;
-    // const singleProductDescription = singleWBCProduct.description;
-    // getProductDetails(productId)
-    //   .then((productDetailsResponse) => {
-    //     console.log(productDetailsResponse);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
   });
 }
 
@@ -68,29 +61,53 @@ function getProductDetails(productId) {
 }
 
 $(() => {
-  $("#submitButton").click(() => {
-    console.log("button clicked");
-    const productID = $("#productSelection option:selected").val();
-    getProductDetails(productID)
-      .then((productDetailsResponse) => {
-        console.log(productDetailsResponse);
-        const responseToString = JSON.stringify(productDetailsResponse);
-        $("#placeholderOnly").append(`<div>${responseToString}</div>`);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    // $("#myselect option:selected").text();
-
-    console.log(productID);
-
-    // const productSummaryInfoTag = $("#productSummaryView").append(
-    //   `<li>${product.name}</li>`
+  // event handler for on change event
+  $("#productSelection").on("change", (event) => {
+    $("#productName").children().remove();
+    $("#productCategory").children().remove();
+    $("#productDescription").children().remove();
+    console.log("test on form select event", event);
+    const selectedProductID = event.currentTarget.value; // this is string, since it is coming from DOM, this is a browser event object
+    console.log("selectedProductID is ", selectedProductID);
+    const findProduct = products.find((product) => {
+      // loop through the product and find the match
+      // console.log("find product from the current target on DOM ", product);
+      if (product.productId == selectedProductID) {
+        console.log("find the exact match on ", product);
+        return true;
+      }
+    });
+    if (findProduct) {
+      console.log("findProduct", findProduct);
+      $("#productName").append(`<p>${findProduct.name}</p>`);
+      $("#productCategory").append(`<p>${findProduct.productCategory}</p>`);
+      $("#productDescription").append(`<p>${findProduct.description}</p>`);
+    }
   });
-  $("#cancelButton").click(() => {
-    console.log("button cancelled");
-    $("#placeholderOnly").empty();
-  });
+});
+
+$("#submitButton").click(() => {
+  console.log("button clicked");
+  const productID = $("#productSelection option:selected").val();
+  getProductDetails(productID)
+    .then((productDetailsResponse) => {
+      console.log(productDetailsResponse);
+      const responseToString = JSON.stringify(productDetailsResponse);
+      $("#placeholderOnly").append(`<div>${responseToString}</div>`);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  // $("#myselect option:selected").text();
+
+  console.log(productID);
+
+  // const productSummaryInfoTag = $("#productSummaryView").append(
+  //   `<li>${product.name}</li>`
+});
+$("#cancelButton").click(() => {
+  console.log("button cancelled");
+  $("#placeholderOnly").empty();
 });
 
 // $(() => {
